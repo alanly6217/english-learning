469265316691ef90f24e6ff722f15d7746373346
alanly6217 <alanly6217@gmail.com>
2026-09-10 13:42:46 +0800

    chore: cache E22-E24 vocabulary lessons offline

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='english-alanly-20260910-learning-v10';
+const CACHE='english-alanly-20260910-learning-v11';
 const ASSETS=[
   './','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png',
   './shell-nav.js',
-  './vocab.html','./vocab.css','./vocab-directory.css','./vocab-data-1.js','./vocab-data-2.js','./vocab-e02.js','./vocab-e03.js','./vocab-e04.js','./vocab-e05.js','./vocab-e06.js','./vocab-e07.js','./vocab-e08.js','./vocab-e09.js','./vocab-e10.js','./vocab-e11.js','./vocab-e12.js','./vocab-e13.js','./vocab-e14.js','./vocab-e15.js','./vocab-e16.js','./vocab-e17.js','./vocab-e18.js','./vocab-e19.js','./vocab-e20.js','./vocab-e21.js','./vocab-lessons.json','./vocab-app-v2.js','./vocab-app-extend.js',
+  './vocab.html','./vocab.css','./vocab-directory.css','./vocab-data-1.js','./vocab-data-2.js','./vocab-e02.js','./vocab-e03.js','./vocab-e04.js','./vocab-e05.js','./vocab-e06.js','./vocab-e07.js','./vocab-e08.js','./vocab-e09.js','./vocab-e10.js','./vocab-e11.js','./vocab-e12.js','./vocab-e13.js','./vocab-e14.js','./vocab-e15.js','./vocab-e16.js','./vocab-e17.js','./vocab-e18.js','./vocab-e19.js','./vocab-e20.js','./vocab-e21.js','./vocab-e22.js','./vocab-e23.js','./vocab-e24.js','./vocab-lessons.json','./vocab-app-v2.js','./vocab-app-extend.js',
   './visit.html','./visit.css','./visit-data-1.js','./visit-data-2.js','./visit-app.js'
 ];
 const INDEX_URL=new URL('./index.html',self.location.href).href;
